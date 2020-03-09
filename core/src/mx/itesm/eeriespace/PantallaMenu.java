33e6d7d69c956b5a782793a404b529d4e17d7d71
Carlos Martinez <cmartinez@tiendapago.com>
2020-03-09 15:49:25 -0600

    Initial commit

---
+++ core/src/mx/itesm/eeriespace/PantallaMenu.java
@@ -0,0 +1,44 @@
+package mx.itesm.eeriespace;
+
+import com.badlogic.gdx.Screen;
+
+class PantallaMenu implements Screen {
+
+    public PantallaMenu(EerieSpace eerieSpace) {
+    }
+
+    @Override
+    public void show() {
+        
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
