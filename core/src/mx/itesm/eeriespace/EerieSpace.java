6693924ee9babbba71a3957d66e1d709fb8188d0
Dpalme <diegopalmerin24@gmail.com>
2020-04-14 19:21:05 -0500

    initial branch commit
    Signed-off-by: Dpalme <diegoPalmerin24@gmail.com>

--- core/src/mx/itesm/eeriespace/EerieSpace.java
+++ core/src/mx/itesm/eeriespace/EerieSpace.java
@@ -1,11 +1,6 @@
 package mx.itesm.eeriespace;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class EerieSpace extends Game {
 
@@ -13,6 +8,6 @@ public class EerieSpace extends Game {
 		public void create () {
 			setScreen(new PantallaMenu(this));
 		}
-
+//
 	}
 
