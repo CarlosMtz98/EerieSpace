5497a9cbc3df01b3d93743b7d8ef801b873ac2bc
Dpalme <diegoPalmerin24@gmail.com>
2020-04-15 15:01:54 -0500

    Comentarios informativos sobre intencionalidad
    Signed-off-by: Dpalme <diegoPalmerin24@gmail.com>

--- core/src/mx/itesm/eeriespace/Meteoro.java
+++ core/src/mx/itesm/eeriespace/Meteoro.java
@@ -11,11 +11,23 @@ public abstract class Meteoro extends Objeto {
     public Meteoro(Texture textura, float x, float y, int daño) {
         super(textura, x, y);
         this.daño = daño;
+        //Un problema con esta estrategia es que si se crea en una esquina y la dirección del
+        //meteoro lo lleva a salirse de la pantalla, nunca lo va a poder destruir el jugador y se
+        //desperdicia el meteoro
         this.angulo = (float) Math.random() - .5f;
     }
 
     public int getDaño() {
         return daño;
+    }
+
+    public void update(float delta) {
+        mover(delta);
+
+        if (sprite.getX() - sprite.getWidth() < 0 || sprite.getX() > Pantalla.ANCHO ||
+                sprite.getY() > Pantalla.ALTO) {
+            //Código para destruirlo
+        }
     }
 
     public void mover(float delta) {
